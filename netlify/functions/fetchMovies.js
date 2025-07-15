20e6a6e419d7ef52dd627f77a63d49c4f1edf64f
Krushna960 <161699252+Krushna960@users.noreply.github.com>
2025-07-15 22:29:26 +0530

    #

--- netlify/functions/fetchMovies.js
+++ netlify/functions/fetchMovies.js
@@ -1,9 +1,10 @@
 // netlify/functions/fetchMovies.js
 
-exports.handler = async function(event, context) {
+exports.handler = async function (event, context) {
   const apiKey = process.env.TMDB_API_KEY;
+
   const genreId = event.queryStringParameters.genre;
-  const lang = event.queryStringParameters.lang;
+  const lang = event.queryStringParameters.lang || 'en';
 
   const url = `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&with_genres=${genreId}&with_original_language=${lang}&sort_by=popularity.desc`;
 
@@ -15,7 +16,7 @@ exports.handler = async function(event, context) {
       statusCode: 200,
       body: JSON.stringify(data)
     };
-  } catch (error) {
+  } catch (err) {
     return {
       statusCode: 500,
       body: JSON.stringify({ error: 'Failed to fetch movies' })
